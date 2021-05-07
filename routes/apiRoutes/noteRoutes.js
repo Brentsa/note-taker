70856e54bb7fc7b20060b64c88226c162bda9966
Solidshots <seanbrent5@gmail.com>
2021-05-07 08:42:08 -0400

    POST route WIP

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -7,4 +7,14 @@ router.get('/notes', (req, res)=>{
     res.json(notes);
 });
 
+//Add a new note to the db file after validating the entry
+router.post('/notes', (req, res)=>{
+    const note = req.body;
+    note.id = notes.length;
+    notes.push(note);
+
+    console.log(note);
+    res.json(notes);
+});
+
 module.exports = router;
